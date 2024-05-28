c49d00a2129d615ed73446b2f45e24a4beda5ff6
Theodoros Savvas <theodo.savvas@gmail.com>
2024-05-28 23:17:06 +0300

    - Improved logging mechanisms

--- src/main/java/com/chain/chainscrape/AppConfig.java
+++ src/main/java/com/chain/chainscrape/AppConfig.java
@@ -2,6 +2,7 @@ package com.chain.chainscrape;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -9,5 +10,17 @@ import org.springframework.context.annotation.Configuration;
 @Data
 public class AppConfig {
     private String appTitle;
+
+    @Bean
+    public CustomLoggingFilter logFilter() {
+        CustomLoggingFilter filter = new CustomLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setIncludeHeaders(false); // You can set this to true to include headers as well
+        filter.setBeforeMessagePrefix("Incoming Request: ");
+        filter.setAfterMessageSuffix(" [END]");
+        return filter;
+    }
 
 }
--- src/main/java/com/chain/chainscrape/Utils.java
+++ src/main/java/com/chain/chainscrape/Utils.java
@@ -18,4 +18,18 @@ public class Utils {
     public static BigDecimal ethDecToBigDecimal(BigInteger ethDecRepresentation, int ethDecimals, int outputDecimals) {
         return new BigDecimal(ethDecRepresentation).divide(BigDecimal.valueOf(Math.pow(10, ethDecimals)), outputDecimals, RoundingMode.HALF_UP);
     }
+
+    /** Checks if an input contains any of the substrings
+     * @param input The string to check
+     * @param toMatch The strings to match
+     * @return True if the input contains any of the substrings
+     */
+    public static boolean containsAny(String input, String... toMatch){
+        for(String s : toMatch){
+            if(input.contains(s)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
